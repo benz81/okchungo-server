be1cc9cdfc459eca5d5c3b19b035c25865dbbfc0
Ben81 <ghafjsdhkd.boss@gmail.com>
2020-08-17 16:05:00 +0900

    as

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const { createUser } = require("../controllers/users");
+const { createUser, loginUser } = require("../controllers/users");
 
 const router = express.Router();
 
 // api/v1/users
 router.route("/").post(createUser);
+router.route("/lohin").post(loginUser);
 
 module.exports = router;
