42bfc2d6f0f1bfcf6d0a0a758c6751231db8c656
Ben81 <ghafjsdhkd.boss@gmail.com>
2020-08-17 16:08:56 +0900

    login

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 // api/v1/users
 router.route("/").post(createUser);
-router.route("/lohin").post(loginUser);
+router.route("/login").post(loginUser);
 
 module.exports = router;
