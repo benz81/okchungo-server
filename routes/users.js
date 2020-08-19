6b49b00b97e66765af6c5078669babdd583f7297
Ben81 <ghafjsdhkd.boss@gmail.com>
2020-08-19 18:02:27 +0900

    add

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const { createUser, loginUser, logout } = require("../controllers/users");
+const {
+  createUser,
+  loginUser,
+  logout,
+  deleteUser,
+  logoutAll,
+  myInfo,
+  getMyInfo,
+  userPhotoUpload,
+  changePasswd,
+  forgotPasswd,
+  resetPasswd,
+} = require("../controllers/users");
 
 const router = express.Router();
 
 // api/v1/users
-router.route("/").post(createUser);
+router
+  .route("/")
+  .post(createUser)
+  .get(auth, getMyInfo)
+  .delete(auth, deleteUser);
 router.route("/login").post(loginUser);
 router.route("/logout").delete(auth, logout);
+router.route("/logout/all").post(auth, logoutAll);
+router.route("/me").get(myInfo);
+router.route("/me/photo").put(userPhotoUpload);
+router.route("/change_passwd").put(changePasswd);
+router.route("/forgotpasswd").post(auth, forgotPasswd);
+router.route("/resetpasswd/:resetPasswdToken").post(auth, resetPasswd);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 // 미들 웨어 로드 찍어주는 로거 app.use에 추가 하는것
 const morgan = require("morgan");
+const logger = require("./middleware/logger.js");
+const errorHandler = require("./middleware/error.js");
 
 // 우리가 파일로 만든것은 항상 npm  패키지 아래쪽에 만들어준다.
 const users = require("./routes/users");
@@ -16,10 +18,13 @@ const app = express();
 app.use(express.json());
 // 먼저 로그 찍어주도록 미들웨어 설정
 app.use(morgan("common"));
+app.use(logger);
 // app.use(express.static(path.join(__dirname, "public")));
 
 // 경로 연결
 app.use("/api/v1/users", users);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5871;
 app.listen(PORT, console.log("서버 가동"));
